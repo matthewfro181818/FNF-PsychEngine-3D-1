223d04f2d0ba4aa746a25c90d7cc0d80c4636ef4
matthewfro181818 <158477647+matthewfro181818@users.noreply.github.com>
2025-10-07 14:54:51 -0400

    Update Stages.hx

--- source/states/stages/Stages.hx
+++ source/states/stages/Stages.hx
@@ -3,11 +3,10 @@ package states.stages;
 import states.stages.objects.*;
 import flixel.addons.effects.FlxSkewedSprite;
 
-class Template extends BaseStage
+class Stages extends BaseStage
 {
+	private var curSong:String = "";
 	var stageCheck:String = 'stage';
-
-	var defaultCamZoom:Float = 1.05;
 
 	public static var curStage:String = '';
 
@@ -21,7 +20,7 @@ class Template extends BaseStage
 				defaultCamZoom = 0.80;
 
 				var bg:FlxSprite = new FlxSprite(-600, -300);
-				bg.frames = Paths.getSparrowAtlasFunk("lily/stuff");
+				bg.frames = Paths.getSparrowAtlas("lily/stuff");
 				bg.animation.addByPrefix("idle", "stage", 24, false);
 				bg.scrollFactor.set(0.7, 0.7);
 				bg.antialiasing = true;
@@ -30,7 +29,7 @@ class Template extends BaseStage
 				add(bg);
 
 				var roadinner = new FlxSprite();
-				roadinner.frames = Paths.getSparrowAtlasFunk("lily/stuff");
+				roadinner.frames = Paths.getSparrowAtlas("lily/stuff");
 				roadinner.animation.addByPrefix("idle", "roadinner", 24, false);
 				roadinner.scrollFactor.set(0.8, 0.8);
 				roadinner.antialiasing = true;
@@ -42,40 +41,28 @@ class Template extends BaseStage
 				add(roadinner);
 
 				var light = new FlxSprite(80, 0);
-				light.frames = Paths.getSparrowAtlasFunk("lily/morestuff");
+				light.frames = Paths.getSparrowAtlas("lily/morestuff");
 				light.animation.addByPrefix("idle", "light", 24, false);
 				light.antialiasing = true;
 				light.scrollFactor.set(0.9, 0.9);
 				add(light);
-				bopSprites.push(function()
-				{
-					light.animation.play("idle", true);
-				});
 				light.animation.play("idle", true);
 
 				var light2 = light.clone();
 				light2.x += 1053;
 				add(light2);
-				bopSprites.push(function()
-				{
-					light2.animation.play("idle", true);
-				});
 				light2.animation.play("idle", true);
 
 				var lilySoil = new FlxSprite(-200, 400);
-				lilySoil.frames = Paths.getSparrowAtlasFunk("lily/morestuff");
+				lilySoil.frames = Paths.getSparrowAtlas("lily/morestuff");
 				lilySoil.animation.addByPrefix("idle", "soil", 24, false);
 				lilySoil.antialiasing = true;
 				lilySoil.scrollFactor.set(0.9, 0.9);
 				add(lilySoil);
-				bopSprites.push(function()
-				{
-					lilySoil.animation.play("idle", true);
-				});
 				lilySoil.animation.play("idle", true);
 
 				var roadfront = new FlxSprite(-600, 581);
-				roadfront.frames = Paths.getSparrowAtlasFunk("lily/stuff");
+				roadfront.frames = Paths.getSparrowAtlas("lily/stuff");
 				roadfront.animation.addByPrefix("idle", "roadfront", 24, false);
 				roadfront.scrollFactor.set(0.9, 0.9);
 				roadfront.antialiasing = true;
@@ -86,7 +73,7 @@ class Template extends BaseStage
 				var glasses = new FlxTypedGroup<FlxSprite>();
 
 				var glass = new FlxSprite(0, 0);
-				glass.frames = Paths.getSparrowAtlasFunk("lily/morestuff");
+				glass.frames = Paths.getSparrowAtlas("lily/morestuff");
 				glass.antialiasing = true;
 				glass.animation.addByPrefix("1", "glassOne", 24, false);
 				glass.animation.addByPrefix("2", "glassTwo", 24, false);
@@ -142,13 +129,13 @@ class Template extends BaseStage
 
 				defaultCamZoom = 0.75;
 
-				var bg:FlxSprite = new FlxSprite(-500, -200).loadGraphic(Paths.getImageFunk('bfStage/bg2'));
+				var bg:FlxSprite = new FlxSprite(-500, -200).loadGraphic(Paths.image('bfStage/bg2'));
 				bg.antialiasing = true;
 				bg.active = false;
 				bg.scrollFactor.set(0.3, 0.3);
 				add(bg);
 
-				var fore:FlxSprite = new FlxSprite(-500, -200).loadGraphic(Paths.getImageFunk('bfStage/fg'));
+				var fore:FlxSprite = new FlxSprite(-500, -200).loadGraphic(Paths.image('bfStage/fg'));
 				fore.antialiasing = true;
 				fore.active = false;
 				add(fore);
@@ -157,26 +144,26 @@ class Template extends BaseStage
 
 				defaultCamZoom = 0.7;
 
-				var bg:FlxSprite = new FlxSprite(-625, -200).loadGraphic(Paths.getImageFunk('ghoti/bg'));
+				var bg:FlxSprite = new FlxSprite(-625, -200).loadGraphic(Paths.image('ghoti/bg'));
 				bg.antialiasing = true;
 				bg.active = false;
 				add(bg);
 			case 'prismaStage':
 				defaultCamZoom = 0.8;
 				curStage = 'prismaStage';
-				var bg = new FlxSprite().loadGraphic(Paths.getImageFunk("prismaStage/space"));
+				var bg = new FlxSprite().loadGraphic(Paths.image("prismaStage/space"));
 				bg.antialiasing = true;
 				bg.scrollFactor.set();
 				bg.screenCenter(XY);
 				add(bg);
 
-				var sun = new FlxSprite().loadGraphic(Paths.getImageFunk("prismaStage/sun"));
+				var sun = new FlxSprite().loadGraphic(Paths.image("prismaStage/sun"));
 				sun.setPosition(FlxG.width / 2 - sun.width / 2, -125);
 				sun.antialiasing = true;
 				sun.scrollFactor.set(0.2, 0.2);
 				add(sun);
 
-				var mtn = new FlxSprite().loadGraphic(Paths.getImageFunk("prismaStage/mountain"));
+				var mtn = new FlxSprite().loadGraphic(Paths.image("prismaStage/mountain"));
 				mtn.setPosition(FlxG.width / 2 - mtn.width / 2, 100);
 				mtn.antialiasing = true;
 				mtn.scrollFactor.set(0.33, 0.33);
@@ -185,26 +172,26 @@ class Template extends BaseStage
 				var skewGrid = new FlxSkewedSprite();
             	var skewScale:Float = -0.1;
 				skewScale = -0.1;
-				skewGrid.loadGraphic(Paths.getImageFunk("prismaStage/tile"));
+				skewGrid.loadGraphic(Paths.image("prismaStage/tile"));
 				skewGrid.antialiasing = true;
 				skewGrid.setPosition(FlxG.width / 2 - skewGrid.width / 2, 600);
 				add(skewGrid);
 			case 'prismaStage2':
 				defaultCamZoom = 0.8;
 				curStage = 'prismaStage';
-				var bg = new FlxSprite().loadGraphic(Paths.getImageFunk("prismaStage/space"));
+				var bg = new FlxSprite().loadGraphic(Paths.image("prismaStage/space"));
 				bg.antialiasing = true;
 				bg.scrollFactor.set();
 				bg.screenCenter(XY);
 				add(bg);
 
-				var sun = new FlxSprite().loadGraphic(Paths.getImageFunk("prismaStage/moon"));
+				var sun = new FlxSprite().loadGraphic(Paths.image("prismaStage/moon"));
 				sun.setPosition(FlxG.width / 2 - sun.width / 2, -175);
 				sun.antialiasing = true;
 				sun.scrollFactor.set(0.2, 0.2);
 				add(sun);
 
-				mtn = new FlxSprite().loadGraphic(Paths.getImageFunk("prismaStage/city"));
+				var mtn:FlxSprite = new FlxSprite().loadGraphic(Paths.image("prismaStage/city"));
 				mtn.setPosition(FlxG.width / 2 - mtn.width / 2, -100);
 				mtn.antialiasing = true;
 				mtn.scrollFactor.set(0.33, 0.33);
@@ -213,14 +200,14 @@ class Template extends BaseStage
 	            var skewScale:Float = -0.1;
 				skewScale = -0.07;
 				var skewGrid = new FlxSkewedSprite();
-				skewGrid.loadGraphic(Paths.getImageFunk("prismaStage/tile2"));
+				skewGrid.loadGraphic(Paths.image("prismaStage/tile2"));
 				skewGrid.antialiasing = true;
 				skewGrid.setPosition(FlxG.width / 2 - skewGrid.width / 2, 600);
 				add(skewGrid);
 			default:
 				defaultCamZoom = 0.9;
 				curStage = 'stage';
-				var bg:FlxSprite = new FlxSprite(-600, -200).loadGraphic(Paths.getImageFunk("stage/stageback2"));
+				var bg:FlxSprite = new FlxSprite(-600, -200).loadGraphic(Paths.image("stage/stageback2"));
 				bg.antialiasing = true;
 				bg.scrollFactor.set(0.9, 0.9);
 				bg.active = false;
@@ -236,7 +223,7 @@ class Template extends BaseStage
 				// stageFront.antialiasing = true;
 				// add(stageFront);
 
-				var stageCurtains:FlxSprite = new FlxSprite(-500, -300).loadGraphic(Paths.getImageFunk("stage/stagecurtains"));
+				var stageCurtains:FlxSprite = new FlxSprite(-500, -300).loadGraphic(Paths.image("stage/stagecurtains"));
 				stageCurtains.setGraphicSize(Std.int(stageCurtains.width * 0.9));
 				stageCurtains.updateHitbox();
 				stageCurtains.antialiasing = true;
